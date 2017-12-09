84830a1c30bee2426e3daa3f516ada325f8c4b24
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2017-12-09 17:03:02 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const port = process.env.PORT;
+const ip = process.env.IP;
+
+//set up the view engine to render ejs
+app.set("view engine", "ejs");
+
+
+//create a route for the search page
+app.get('/',function(req,res){
+    res.render('search');
+});
+
+//create route for the results page
+app.get('/results',function(req,res){
+ var searchquery = req.query.search;
+ const key = '&apikey=thewdb';
+ var url = "http://www.omdbapi.com/?s="+searchquery+key;
+ request(url,function(error,response,body){
+    if(!error && response.statusCode ==200){
+        var data = JSON.parse(body);
+        res.render('results',{data:data});
+    } 
+ });
+});
+
+app.listen(port,ip,function(){
+   console.log("The server is running on port :"+port);
+});
