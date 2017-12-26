91a9ef688070f463b029df7614d1e347ebbddb32
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2017-12-25 22:02:08 -0600

    updated files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const request = require('request');
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const ip = process.env.IP;
 
 //set up the view engine to render ejs
